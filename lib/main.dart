af8a38360b733dc41b28b11270741e4b70cdae64
Selvaganesh <146106646+selvaganesh0@users.noreply.github.com>
2024-07-27 12:31:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: ChatScreen(),
     );
   }
@@ -21,12 +22,25 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   TextEditingController _textController = TextEditingController();
-  int count=0;
   List<Message> _messages = [
-    Message(isMe:false, text:"this is a calculator"),
-    Message(isMe:true, text:"Just type your expression"),
-
+    Message(isMe: true, text: "This is a calculator"),
+    Message(isMe: false, text: "Just type your expression"),
   ];
+
+  void _handleSendMessage(String message) {
+    setState(() {
+      _messages.add(Message(isMe: true, text: message));
+      try {
+        Parser p = Parser();
+        Expression exp = p.parse(message);
+        ContextModel cm = ContextModel();
+        double eval = exp.evaluate(EvaluationType.REAL, cm);
+        _messages.add(Message(isMe: false, text: eval.toString()));
+      } catch (e) {
+        _messages.add(Message(isMe: false, text: "Error"));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,20 +54,12 @@ class _ChatScreenState extends State<ChatScreen> {
             child: ListView.builder(
               itemCount: _messages.length,
               itemBuilder: (context, index) {
-                return MessageWidget(message:_messages[index]);
+                return MessageWidget(message: _messages[index]);
               },
             ),
           ),
           InputWidget(
-            onSendMessage: (String message) {
-              setState(() {
-                count++;
-                if(count%2==0)
-                  _messages.add(Message(isMe:false, text: message));
-                else
-                  _messages.add(Message(isMe:true, text: message));
-              });
-            },
+            onSendMessage: _handleSendMessage,
           ),
         ],
       ),
@@ -65,7 +71,7 @@ class Message {
   final bool isMe;
   final String text;
 
-  Message({required this.isMe,required this.text});
+  Message({required this.isMe, required this.text});
 }
 
 class MessageWidget extends StatelessWidget {
@@ -77,16 +83,16 @@ class MessageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: message.isMe ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: message.isMe ? Colors.blue : Colors.black87,
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        child: Text(
-          message.text,
-          style: TextStyle(color: Colors.white),
+      child: Card(
+        color: message.isMe ? Colors.blue : Colors.black87,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Text(
+            message.text,
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
@@ -104,7 +110,7 @@ class InputWidget extends StatefulWidget {
 
 class _InputWidgetState extends State<InputWidget> {
   final TextEditingController _textController = TextEditingController();
-  String res="";
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -115,10 +121,11 @@ class _InputWidgetState extends State<InputWidget> {
             child: TextField(
               controller: _textController,
               keyboardType: TextInputType.number,
-
               decoration: InputDecoration(
-                  hintText: 'Type your message...',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25)))
+                hintText: 'Type your message...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                ),
               ),
             ),
           ),
@@ -129,20 +136,7 @@ class _InputWidgetState extends State<InputWidget> {
               if (message.isNotEmpty) {
                 widget.onSendMessage(message);
                 _textController.clear();
-                Parser p = Parser();
-                Expression exp = p.parse(message);
-                ContextModel cm = ContextModel();
-
-                double eval = exp.evaluate(EvaluationType.REAL, cm);
-                try {
-                  widget.onSendMessage(eval.toString());
-                }
-                catch(e)
-                {
-                  widget.onSendMessage("error");
-                }
               }
-
             },
           ),
         ],
